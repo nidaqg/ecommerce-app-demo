247ba33812578d80784edaf604435ea82acc39f3
nqg <nidaghuman@gmail.com>
2021-12-23 14:44:27 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import {Homepage} from './Pages/Homepage/Homepage';
+import React from "react";
+import { Homepage } from "./Pages/Homepage/Homepage";
 import "./app.scss";
+import { BrowserRouter as Router, 
+  Route, 
+  Routes 
+} from "react-router-dom";
+
+
+const HatsPage = () => {
+  return (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div>
-    <Homepage/>
-      
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/hats" element={<HatsPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
