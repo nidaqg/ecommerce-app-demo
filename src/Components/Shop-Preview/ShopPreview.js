cc839ff6e8e06e84c25d041e1c8caff37bc2a62c
nqg <nidaghuman@gmail.com>
2021-12-28 16:53:05 -0500

    connected collection data to shop page. styling for collection previews

--- src/Components/Shop-Preview/ShopPreview.js
+++ src/Components/Shop-Preview/ShopPreview.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './ShopPreviewStyles.scss';
+import React from "react";
+import "./ShopPreviewStyles.scss";
+import { PreviewItem } from "../Preview-Item/PreviewItem";
 
-export const ShopPreview =({title, items}) => {
-    return (
-        <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-         {
-          items.map(item => (
-              <div key ={item.id}>{item.name}</div>
-          ))
-         }
-        </div>
-        </div>
-    )
-}
-
+export const ShopPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map((item) => (
+              <PreviewItem
+              key={item.id}
+              name={item.name}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              id={item.id}
+              />
+          ))}
+      </div>
+    </div>
+  );
+};
