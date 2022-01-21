e07c0123397989a27b22013de8ca26950a573dbc
nqg <nidaghuman@gmail.com>
2022-01-21 12:30:26 -0500

    used rduce method to get total items quantity in cart

--- src/Components/Cart-Component/Cart.js
+++ src/Components/Cart-Component/Cart.js
@@ -7,17 +7,25 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../Redux/Cart/CartActions";
 
-const CartIcon = ({toggleCartHidden}) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
-      <ShoppingIcon className="shopping-icon"/>
-      <span className="item-count">0</span>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
+
+//use reduce function to find total quantity
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  ),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
