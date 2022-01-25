ee4e0d2b9599dcf6a9acde38f3033d89fe2ad5dc
nqg <nidaghuman@gmail.com>
2022-01-25 09:16:07 -0500

    added cart toggle to go to checkout button

--- src/Components/Cart-DropDown/Cart-DropDown.js
+++ src/Components/Cart-DropDown/Cart-DropDown.js
@@ -7,12 +7,18 @@ import { CartItem } from "../Cart-Item/CartItem";
 
 //import selector
 import { selectCartItems } from "../../Redux/Cart/CartSelectors";
+import {toggleCartHidden} from "../../Redux/Cart/CartActions";
 
 import { connect } from "react-redux";
 
-const CartDropDown = ({ cartItems }) => {
+const CartDropDown = ({ cartItems, toggleCartHidden }) => {
 
   let navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/checkout");
+    toggleCartHidden();
+  }
 
   return (
     <div className="cart-dropdown">
@@ -24,7 +30,7 @@ const CartDropDown = ({ cartItems }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={()=> {navigate('/checkout')}}>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={handleClick}>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
@@ -33,4 +39,8 @@ const mapStateToProps = (state) => ({
   cartItems: selectCartItems(state),
 });
 
-export default connect(mapStateToProps)(CartDropDown);
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartDropDown);
